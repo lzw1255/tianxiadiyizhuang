d4dff03651351c07a9ca3ab3a4d25ade2ed3945d
root <root@outlook.com>
2019-05-04 20:00:22 +0800

    第一次提交

---
+++ src/main/java/com/itheima/domain/Main.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @Author LZW
+ * @DATA 2019/5/4 19:56
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("第一个版本");
+    }
+}
